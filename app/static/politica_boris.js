14147b8ef7820d8ba91ae6fb10b57f2c5e98a127
Esteban <erezzoni@yandex.com>
2021-06-08 18:29:19 -0300

    js

---
+++ app/static/politica_boris.js
@@ -0,0 +1,18 @@
+(function(){
+    // Compruebo si estoy en la página del detalle de una orden
+	if (window.location.pathname.toUpperCase().indexOf('POLITICA') === -1) {
+        // Almaceno el store_id en una variable.
+        let storeId = LS.store.id;
+        // Almaceno el order_id en una variable
+        //let orderId = document.URL.match(/\d+/)[0];
+        // Creo y agrego el nuevo botón
+        let referenceNode = document.querySelector('.boris');
+        let myButton = document.createElement('a');
+        myButton.classList.add('btn','btn-primary', 'btn-small', 'full-width-xs', 'm-top', 'pull-right', 'pull-none-xs');
+
+        myButton.setAttribute('href',`https://frontprod.borisreturns.com?store_id=${storeId}`);
+        myButton.setAttribute('target', '_blank');
+        myButton.textContent = 'Cambiar / Devolver un producto (politica)';
+        referenceNode.append(myButton);
+    }
+})();
